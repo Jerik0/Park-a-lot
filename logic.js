4f18aef15bd70db9c7468250ee34fbffbb1768b4
Jeriko <jcarrico1985@live.com>
2017-09-26 12:52:03 -0500

    added logic to randomly pick space from corresponding spaces. will update to include ability to choose only available spaces next.

--- logic.js
+++ logic.js
@@ -5,12 +5,11 @@
   let mediumSelected = $('#medium');
   let largeSelected = $('#large');
   let parkingBtn = $('#get-parking-btn');
-  let radioVal;
   let carInfo = $('#car-info-container');
 
   const getRadioVal = (form, name) => {
-    let radios = form.elements[name];
     let val;
+    let radios = form.elements[name];
 
     for(let i=0, len = radios.length; i<len; i++) {
       if(radios[i].checked) {
@@ -22,33 +21,49 @@
   };
 
   parkingBtn.click(() => {
+    let radioVal;
     radioVal = getRadioVal(document.getElementById('car-size'), 'size');
     console.log(radioVal);
-    switch(radioVal) {
-      case 'small':
-        carInfo.html(`You can park in any space!`);
-        break;
-      case 'medium':
-        carInfo.html(`You may park in any space numbered from 51 to 200`);
-        break;
-      case 'large':
-        carInfo.html('You may only park in large spaces.');
-        break;
-    }
+    receiptPrint(radioVal);
   });
-
-
 
   const parkLogic = (spaces) => {
     const smallSpaces = spaces.small;
     const mediumSpaces = spaces.medium;
     const largeSpaces = spaces.large;
+    //TODO subtract one from the corresponding space section in JSON file.
+    //TODO Then send the selected space to receiptPrint()
+    //TODO Then call parkDisplay again to display total spaces again.
+  };
+
+  const spacePicker = (size) => {
+    switch(size) {
+      case 'small':
+        let smallPicked = Math.floor(Math.random() * (50-1) + 1);
+        return smallPicked;
+        break;
+      case 'medium':
+        let medPicked = Math.floor(Math.random() * (200-51) + 51);
+        return medPicked;
+        break;
+      case 'large':
+        let largePicked = Math.floor(Math.random() * (200-101) + 101);
+        return largePicked;
+        break;
+    }
   };
 
   const parkDisplay = (total) => {
 
     console.log(`There are ${total} spaces available.`);
 
+    //TODO Display how many small medium and large spaces available
+
+  };
+
+  const receiptPrint = (size) => {
+    //TODO Print number of the space assigned to customer
+    console.log("Your space number is " + spacePicker(size));
   };
 
   $.ajax('spaces-data.json').done(function(data, status) {
